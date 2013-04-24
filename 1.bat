7295e5f29e78b8674563a3b093ceea7827e5c3f0
Alek <falkenbach@ya.ru>
2013-04-24 12:04:36 +0700

    first commit

---
+++ 1.bat
@@ -0,0 +1,5 @@
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/biasot/ppdemo.git
+git push -u origin master
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace PaypalMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult PostToPayPal(string item , string amount, string custom)
+        {
+            PaypalMVC.Models.Paypal paypal = new Models.Paypal();
+            paypal.cmd = "_s-xclick";
+            paypal.business = ConfigurationManager.AppSettings["BusinessAccountKey"];
+
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["UseSandbox"]);
+            if (useSandbox)
+                ViewBag.actionURl = "https://www.sandbox.paypal.com/cgi-bin/webscr";
+            else
+                ViewBag.actionURl = "https://www.paypal.com/cgi-bin/webscr";
+
+            paypal.cancel_return = System.Configuration.ConfigurationManager.AppSettings["CancelURL"];
+            paypal.@return = ConfigurationManager.AppSettings["ReturnURL"];
+            paypal.notify_url = ConfigurationManager.AppSettings["NotifyURL"];
+
+            paypal.currency_code = ConfigurationManager.AppSettings["CurrencyCode"];
+
+            paypal.item_name = item;
+            paypal.amount = amount;
+            paypal.custom = custom;
+            return View(paypal);
+        }
+
+        public ActionResult IPN()
+        {
+            // Receive IPN request from PayPal and parse all the variables returned
+            var formVals = new Dictionary<string, string>();
+            formVals.Add("cmd", "_notify-validate");
+ 
+            // if you want to use the PayPal sandbox change this from false to true
+            string response = GetPayPalResponse(formVals);
+ 
+            if (response == "VERIFIED")
+            {
+                string transactionID = Request["txn_id"];
+                string sAmountPaid = Request["mc_gross"];
+                string userID = Request["custom"];
+ 
+                //validate the order
+                Decimal amountPaid = 0;
+                Decimal.TryParse(sAmountPaid, out amountPaid);
+ 
+                if (sAmountPaid == "5")
+                {
+                    // take the information returned and store this into a subscription table
+ 
+                    return View();
+ 
+                }
+                else
+                {
+                    // let fail - this is the IPN so there is no viewer
+                }
+            }
+ 
+            return View();
+        }
+
+        string GetPayPalResponse(Dictionary<string, string> formVals)
+        {
+ 
+            // Parse the variables
+            // Choose whether to use sandbox or live environment
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["UseSandbox"]);
+            string paypalUrl = useSandbox ? "https://www.sandbox.paypal.com/cgi-bin/webscr" : "https://www.paypal.com/cgi-bin/webscr";
+ 
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(paypalUrl);
+ 
+            // Set values for the request back
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+ 
+            byte[] param = Request.BinaryRead(Request.ContentLength);
+            string strRequest = Encoding.ASCII.GetString(param);
+ 
+            StringBuilder sb = new StringBuilder();
+            sb.Append(strRequest);
+ 
+            foreach (string key in formVals.Keys)
+            {
+                sb.AppendFormat("&{0}={1}", key, formVals[key]);
+            }
+            strRequest += sb.ToString();
+            req.ContentLength = strRequest.Length;
+ 
+            //for proxy
+            //WebProxy proxy = new WebProxy(new Uri("http://urlort#");
+            //req.Proxy = proxy;
+            //Send the request to PayPal and get the response
+            string response = "";
+            using (StreamWriter streamOut = new StreamWriter(req.GetRequestStream(), System.Text.Encoding.ASCII))
+            {
+ 
+                streamOut.Write(strRequest);
+                streamOut.Close();
+                using (StreamReader streamIn = new StreamReader(req.GetResponse().GetResponseStream()))
+                {
+                    response = streamIn.ReadToEnd();
+                }
+            }
+ 
+            return response;
+       }
+
+    }
+}
